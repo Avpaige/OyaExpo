5516701a2495beb98ca5c53d64fa05e8f80471cc
Ari Virgil-Paige <avpaige@gmail.com>
2019-11-11 19:12:03 -0800

    unbroken chat

--- OyaWorkingChat/App.js
+++ OyaWorkingChat/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import { View, Text, AsyncStorage, ActivityIndicator } from 'react-native';
+import { View, Text, AsyncStorage } from 'react-native';
 import SocketIOClient from 'socket.io-client';
 import { GiftedChat } from 'react-native-gifted-chat';
-import axios from "axios";
 
 const USER_ID = '@userId';
-
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: true,
       messages: [],
-      userId: null,
-      usersInChat: []
+      userId: null
     };
 
     this.determineUser = this.determineUser.bind(this);
@@ -25,26 +21,6 @@ class Main extends React.Component {
     this.socket = SocketIOClient('http://localhost:3000');
     this.socket.on('message', this.onReceivedMessage);
     this.determineUser();
-  }
-
-
-  componentDidMount() {
-    fetch("mongodb://localhost:27017/chat")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            messages: result.items
-          });
-        },
-    (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
   }
 
   /**
@@ -82,7 +58,7 @@ class Main extends React.Component {
    * When a message is sent, send the message to the server
    * and store it in this component's state.
    */
-  onSend(messages = []) {
+  onSend(messages=[]) {
     this.socket.emit('message', messages[0]);
     this._storeMessages(messages);
   }
@@ -105,7 +81,6 @@ class Main extends React.Component {
       return {
         messages: GiftedChat.append(previousState.messages, messages),
       };
-
     });
   }
 }
